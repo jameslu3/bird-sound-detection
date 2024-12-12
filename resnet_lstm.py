1df22110e0dd80678b99f78e3223d11311781f5b
jameslu3 <jameslu2003@gmail.com>
2024-12-12 16:38:07 -0500

    Provided code fixes to make training possible

--- resnet_lstm.py
+++ resnet_lstm.py
@@ -4,7 +4,8 @@ import keras_cv
 
 def create_resnet_lstm_model(CFG):
     # Create an input layer for the model
-    inp = keras.layers.Input(shape=(CFG.time_steps, CFG.img_size[1], CFG.img_size[0], 3))
+    inp = keras.layers.Input(shape=(CFG.time_steps, CFG.img_size[0], CFG.img_size[1] // CFG.time_steps, 3))
+    #inp = keras.layers.Input(shape=(None, None, 3))
     
     # Load the ResNet50 backbone
     backbone = keras_cv.models.ResNet50Backbone.from_preset(
